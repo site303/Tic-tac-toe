106c677b2d321a79292e5af097967b7bcc00942b
site303 <buranuranuragan@mail.ru>
2025-01-26 09:48:52 +0300

    add img for btn restart

--- src/components/RestartBtn.js
+++ src/components/RestartBtn.js
@@ -1,10 +1,11 @@
 import React from "react";
+import restartImg from "../icons/restart.png";
 import styles from "./TicTacToe.module.css";
 
 export default function RestartBtn({ handleClear }) {
   return (
     <button className={styles.restartBtn} onClick={() => handleClear()}>
-      Restart
+      <img src={restartImg} class={styles.restartImg} alt="restart"/>
     </button>
   );
 }
